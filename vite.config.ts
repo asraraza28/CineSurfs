55fa63a90a7efa8830f8dbe3d682488a27615632
Asrar Raza <asrarraza325@gmail.com>
2025-08-17 00:01:11 +0530

    Build for Netlify, ensure _redirects and 404.html present

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import react from '@vitejs/plugin-react-swc';
 import path from 'path';
 
 export default defineConfig(({ mode }) => {
-  // Load env file based on `mode`
+  // Load environment variables based on the current mode
   const env = loadEnv(mode, process.cwd(), '');
 
   return {
+    // Use VITE_BASE_URL env variable or default to empty string for base path
     base: env.VITE_BASE_URL || '',
     plugins: [react()],
     resolve: {
